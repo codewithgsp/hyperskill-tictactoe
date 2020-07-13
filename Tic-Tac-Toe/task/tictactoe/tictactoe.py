2560fb70b403706c25eb414cebcddef661e72b3e
codewithgsp <codewithgsp@gmail.com>
2020-07-13 18:06:08 +0200

    stage -5 complete

--- Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -2,40 +2,21 @@
 
 class TicTacToe:
 
-    def __init__(self, userinput):
-        self.user_input = userinput
-        self.game = [['*' for _ in range(3)] for _ in range(3)]
+    def __init__(self):
+        self.game = [[' ' for _ in range(3)] for _ in range(3)]
         self.count_x = 0
         self.count_o = 0
         self.count_u = 0
-
-    def split_input_to_matrix(self):
-        count = 0
-        for i in range(3):
-            for j in range(3):
-                self.game[i][j] = self.user_input[count]
-                count += 1
+        self.element_status = 1
 
     def display_dash(self):
         print('-' * 9)
 
-    def display_game(self, initial=False):
+    def display_game(self):
         self.display_dash()
-        if initial:
-            self.split_input_to_matrix()
         for row in self.game:
             print('|', " ".join(row), '|')
         self.display_dash()
-
-    def check_count_of_xo(self):
-        for i in range(len(self.game)):
-            for j in range(len(self.game)):
-                if self.game[i][j] == 'X':
-                    self.count_x += 1
-                elif self.game[i][j] == 'O':
-                    self.count_o += 1
-                elif self.game[i][j] == '_':
-                    self.count_u += 1
 
     def check_win_scenario(self, symbol):
         # check for each row for a symbol
@@ -54,58 +35,56 @@ class TicTacToe:
         return False
 
     def analyse_result(self):
-        self.check_count_of_xo()
-        if self.check_win_scenario('X') and self.check_win_scenario('O'):
-            print('Impossible')
-            return
         if self.check_win_scenario('X'):
             print('X wins')
-            return
+            return True
         if self.check_win_scenario('O'):
             print('O wins')
-            return
-        if self.count_u == 0:
-            print('Draw')
-            return
-        if abs(self.count_x - self.count_o) >= 2:
-            print('Impossible')
-            return
-        if 0 <= abs(self.count_x - self.count_o) <= 1 and self.count_u > 0:
-            print('Game not finished')
-            return
+            return True
+        return False
 
     def add_element(self):
         while True:
             # check for non numeric characters and raise alert
-            try:
-                x, y = map(int, input('Enter the coordinates:').split())
-            except ValueError:
-                print('You should enter numbers!')
-                continue
+            if self.element_status <= 9:
+                try:
+                    x, y = map(int, input('Enter the coordinates:').split())
+                except ValueError:
+                    print('You should enter numbers!')
+                    continue
 
-            # check for x or y co-ordinates for more than value 3
-            if not (1 <= x <= 3 and 1 <= y <= 3):
-                print('Coordinates should be from 1 to 3!')
-                continue
+                # check for x or y co-ordinates for more than value 3
+                if not (1 <= x <= 3 and 1 <= y <= 3):
+                    print('Coordinates should be from 1 to 3!')
+                    continue
 
-            # assign the value as per matrix
-            i, j = 3 - y, x - 1
+                # assign the value as per matrix
+                i, j = 3 - y, x - 1
 
-            # search for the cell location and substitute
-            if self.game[i][j] == '_':
-                self.game[i][j] = 'X'
-                break
+                # search for the cell location and substitute
+
+                if self.game[i][j] == ' ':
+                    if self.element_status % 2 == 1:
+                        self.game[i][j] = 'X'
+                    elif self.element_status % 2 == 0:
+                        self.game[i][j] = 'O'
+                    self.element_status += 1
+                    break
+                else:
+                    print('This cell is occupied! Choose another one!')
+                    continue
             else:
-                print('This cell is occupied! Choose another one!')
-                continue
+                print('Draw')
+                exit(0)
 
     def play(self):
-        self.display_game(initial=True)
-        # self.analyse_result()
-        self.add_element()
         self.display_game()
+        while True:
+            self.add_element()
+            self.display_game()
+            if self.analyse_result():
+                break
 
 
-user_input = input('Enter cells:')
-tic_tac_toe = TicTacToe(user_input)
+tic_tac_toe = TicTacToe()
 tic_tac_toe.play()
